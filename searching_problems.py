34fe3eb95ddb0e9e23ccd71ccd9755412e6161a6
gracephilip <gphilip@fwparker.org>
2018-03-01 13:24:56 -0600

    Problems done

--- searching_problems.py
+++ searching_problems.py
@@ -11,10 +11,37 @@ def split_line(line):
 #1.  (7pts) Write code which finds and prints the longest
 # word in the provided dictionary.  If there are more
 # than one longest word, print them all.  (read the file line by line to accomplish this task)
+file = open('search_files/dictionary.txt', 'r')
+all_words = []
+
+for line in file:
+    words = split_line(line)
+    for word in words:
+        all_words.append(word)
+
+lengths = [len(x) for x in all_words]
+print(all_words[lengths.index(max(lengths))])
+
+file.close()
 
 #2.  (7pts)  Write code which finds
 #  The total word count AND average word length of "AliceInWonderLand.txt"
 
+file = open('search_files/AliceInWonderland.txt')
+
+word_count = 0
+word_len = []
+average = 0
+
+for line in file:
+    words = split_line(line)
+    for word in words:
+        word_count += 1
+        word_len.append(len(word))
+
+print(word_count)
+
+print(sum(word_len) / len(word_len))
 
 
 # CHOOSE ONE OF THE FOLLOWING TWO PROBLEMS
@@ -22,6 +49,27 @@ def split_line(line):
 #3 (13pts)  How many times does "Cheshire" occur in"AliceInWonderLand.txt"?
 # How many times does "Cat" occur?
 # How many times does "Cheshire" immediately followed by "Cat" occur?
+
+cheshire_num = 0
+Cheshire = False
+file = open('search_files/AliceInWonderland.txt', 'r')
+all_words = []
+
+for line in file:
+    words = split_line(line)
+    for word in words:
+        all_words.append(word)
+
+for word in all_words:
+    if word == "Cheshire":
+        Cheshire = True
+
+
+for i in range(len(all_words)):
+    if all_words[i] == "Cheshire":
+        if all_words[i + 1].upper() == "CAT":
+            cheshire_num += 1
+print(cheshire_num)
 
 
 #### OR #####

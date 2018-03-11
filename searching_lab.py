fc9d10cd6da452f02cf3fa302fbdd5e9f87e2172
gracephilip <gphilip@fwparker.org>
2018-03-11 17:56:40 -0500

    Finished lab

--- searching_lab.py
+++ searching_lab.py
@@ -15,6 +15,16 @@ def linear_search(word, dictionary_list, line_num):
         print("Line", line_num, "possible misspelled word:", word)
 
 
+def binary_search(word, dictionary_list, line_num):
+    key = word
+    for words in dictionary_list:
+        if words == key.upper():
+            break
+    else:
+        print("Line", line_num, "possible misspelled word:", word)
+
+
+
 
 dictionary_list = []
 
@@ -40,8 +50,29 @@ file.close()
 
 print("--- Binary Search ---")
 
+
+found = False
+lower_bound = 0
+upper_bound = len(dictionary_list) - 1
 file = open('search_files/AliceInWonderland.txt')
 
 
 
+while not found and lower_bound <= upper_bound:
+    middle_pos = (upper_bound + lower_bound) // 2
+    if dictionary_list[middle_pos].upper() < word.upper():
+        lower_bound = middle_pos + 1
+    elif dictionary_list[middle_pos].upper() > word.upper():
+        upper_bound = middle_pos -1
+    else:
+        found = True
+if not found:
+    print("Line ", line, "possible misspelled word: ", word)
+
+line_num = 0
+for line in file:
+    words = split_line(line)
+    line_num += 1
+    for word in words:
+        binary_search(word, dictionary_list, line_num)
 

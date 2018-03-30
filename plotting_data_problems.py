13c6f2a2a430cce845b6c1ac3564b51b5726f791
gracephilip <gphilip@fwparker.org>
2018-03-30 10:19:14 -0500

    commit

--- plotting_data_problems.py
+++ plotting_data_problems.py
@@ -1,3 +1,6 @@
+import csv
+import matplotlib.pyplot as plt
+
 
 # MATPLOTLIB PROBLEM # 1
 # Chicago Public Library Computer Usage (15pts)
@@ -8,6 +11,32 @@
 # label the x with the month.  Rotate the text so we can read it.  (see example problem).  Use the tight_fit command to show all text.
 # label axes, title the graph as necessary.
 
+
+with open("files/Libraries_-_2017_Visitors_by_Location.csv") as f:
+    reader = csv.reader(f)  # make a reader object
+    data = list(reader)  # casting the reader object as a list
+
+total_visitors = []
+
+month_names = data.pop(0)[1:-1]
+
+for i in range(1, 13):
+    visitors = [x[i] for x in data][1:-1]
+    visitors = [int(x) for x in visitors]
+    total_visitors.append(sum(visitors))
+
+print(total_visitors)
+
+months = [x for x in range(1, 13)]
+
+plt.figure(1, figsize=(10, 6), tight_layout=True, facecolor="lightblue")
+plt.bar(months, total_visitors, color="blue")
+plt.xticks(months, month_names, rotation=90, fontsize=8)
+plt.title("Total Monthly Visitors - 2017", fontsize=25)
+plt.ylabel("Number of Visitors")
+plt.show()
+
+
 # MATPLOTLIB PROBLEM # 2 
 # Chicago Public Transit Usership Graph (20pts)
 # go to https://data.cityofchicago.org/Transportation/CTA-Ridership-Annual-Boarding-Totals/w8km-9pzd
@@ -17,3 +46,29 @@
 # Make each line, points, and color different for the four graphs.
 # Make a legend to identify each line.
 # Label axes and give your graph a title.  Change it in any other way you see necessary to give it a clean look.
+
+with open("files/CTA_-_Ridership_-_Annual_Boarding_Totals.csv") as f:
+    reader = csv.reader(f)  # make a reader object
+    data = list(reader)  # casting the reader object as a list
+
+
+total_num = []
+
+cta_names = data.pop(0)[1:-1]
+
+for i in range(1, 6):
+    visitors = [x[i] for x in data][1:-1]
+    visitors = [int(x) for x in visitors]
+    total_num.append(sum(visitors))
+
+year_num = [x[0] for x in data]
+
+for line in data:
+    total_by_year = [x[i] for x in data]
+
+
+
+
+print(total_num)
+
+months = [x for x in range(1, 13)]
--- turtle_recursion.py
+++ turtle_recursion.py
@@ -2,12 +2,108 @@
 Turtle Recursion (33pts)
 
 1)  Using the turtle library, create the H fractal pattern according to the file shown in the data folder. (15pts)
+'''
+import turtle
 
+my_turtle = turtle.Turtle()
+my_turtle.speed(0)
+my_turtle.shape("turtle")
+my_turtle.showturtle()
+my_screen = turtle.Screen()
+
+my_screen.bgcolor("white")
+
+def recursive_hfractal(x, y, size, depth):
+    if depth > 0:
+        my_turtle.up()
+        my_turtle.goto(x, y)
+        my_turtle.down()
+        my_turtle.goto(x + size / 2, y)
+        my_turtle.goto(x - size / 2, y)
+        my_turtle.up()
+        my_turtle.goto(x + size / 2, y + size / 2)
+        my_turtle.down()
+
+        my_turtle.goto(x + size / 2, y - size / 2)
+
+        my_turtle.up()
+        my_turtle.goto(x - size / 2, y + size / 2)
+        my_turtle.down()
+        my_turtle.goto(x - size / 2, y - size / 2)
+
+        recursive_hfractal(x + size / 2, y + size / 2, size / 2, depth - 1)
+        recursive_hfractal(x + size / 2, y - size / 2, size / 2, depth - 1)
+
+        recursive_hfractal(x - size / 2, y - size / 2, size / 2, depth - 1)
+        recursive_hfractal(x - size / 2, y + size / 2, size / 2, depth - 1)
+
+
+recursive_hfractal(0, 0, 300, 1)
+
+
+
+my_screen.clear()
+
+'''
 2)  Using the turtle library, create any of the other recursive patterns in the data folder. 
 Challenge yourself to match your pattern as closely as you can to the image.  (15pts)
 Note:  The Koch snowflake shows step by step building of the fractal.  
        The rectangle fractal example shows 4 possible drawings of the same fractal (choose any one)
+'''
+my_turtle.hideturtle()
+def koch_fractal(size, depth, my_turtle, depth_init):
+    if depth > 0:
+        my_turtle.down()
+        my_turtle.begin_fill()
+        my_turtle.forward(size / 2)
+        my_turtle.left(120)
+        #my_turtle.up()
+        my_turtle.forward(size)
+        my_turtle.left(120)
+        my_turtle.forward(size)
+        my_turtle.left(120)
+        my_turtle.forward(size / 2)
+        my_turtle.end_fill()
+        #my_turtle.down()
 
+        if depth == depth_init:
+            my_turtle.left(180)
+            my_turtle1 = my_turtle.clone()
+            koch_fractal(size / 2, depth - 1, my_turtle1, depth_init)
+
+            my_turtle.left(180)
+
+        my_turtle.up()
+        my_turtle.forward(size / 2)
+        my_turtle.left(120)
+        my_turtle.forward(size / 2)
+        my_turtle.right(180)
+        my_turtle2 = my_turtle.clone()
+        koch_fractal(size / 2, depth - 1, my_turtle2, depth_init)
+
+        my_turtle.up()
+        my_turtle.left(180)
+        my_turtle.forward(size / 2)
+        my_turtle.left(120)
+        my_turtle.forward(size / 2)
+        my_turtle.left(180)
+        my_turtle3 = my_turtle.clone()
+        koch_fractal(size / 2, depth - 1, my_turtle3, depth_init)
+
+
+my_turtle.up()
+my_turtle.goto(0, -150)
+koch_fractal(300, 6, my_turtle, 6)
+my_screen.exitonclick()
+
+
+
+
+
+
+
+
+'''
 3)  Create your own work of art with a repeating pattern of your making.  
 It must be a repeated pattern using recursion. Snowflakes, trees, and spirals are a common choice.  
 Take this one as seriously as the points indicate.  Play around.  (3pt) 
